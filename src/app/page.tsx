594b03cc8c8d63ba94fc987842ad291d580bfcdb
frantribu <franmartinmdq@gmail.com>
2023-10-30 19:23:16 -0300

    primer commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import ListaDeBlogs from '@/components/ListaDeBlogs'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <ListaDeBlogs />
+
+    </main>
+  )
+}
